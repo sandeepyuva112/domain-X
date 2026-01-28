2f34f77274f35e316905b2abc0504fae1e036923
Sandeep <sandeep.yuva112@gmail.com>
2026-01-28 11:44:17 +0530

    commit1

--- folder/demo_2.py
+++ folder/demo_2.py
@@ -1,1 +1,10 @@
-print("hi"*5)
+import streamlit as st
+st.title("Demo: Simple Streamlit App")
+st.write("This is a simple Streamlit application demonstrating basic functionality.")
+st.number_input("Enter a number:",key="input1")
+st.number_input("Enter a number:",key="input2")
+st.button("Submit",key="name_button1"),
+if st.button=="name_button1":
+    st.write(name_input1+name_input2)
+
+
